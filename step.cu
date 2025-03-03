b0585db1163acd4c983ba353e1bb242017c66fb4
Dakoro <david.korobetski@gmail.com>
2025-03-03 14:49:11 +0100

    feat: add cuda version of step

--- step.cu
+++ step.cu
@@ -8,6 +8,7 @@
 #include <cfloat>
 #include <random>
 #include <cuda_runtime.h>
+#include <omp.h>
 
 static inline void check(cudaError_t err, const char* context) {
     if (err != cudaSuccess) {
@@ -42,6 +43,165 @@ __global__ void mykernel(float* r, const float* d, int n) {
     r[n*i + j] = v;
 }
 
+__global__ void mykernel_v2(float* r, const float* d, int n) {
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
+    if (i >= n || j >= n)
+        return;
+    float v = HUGE_VALF;
+    for (int k = 0; k < n; ++k) {
+        float x = d[n*j + k];
+        float y = d[n*k + i];
+        float z = x + y;
+        v = min(v, z);
+    }
+    r[n*j + i] = v;
+}
+
+__global__ void mykernel_v3(float* r, const float* d, int n, int nn) {
+    int ia = threadIdx.x;
+    int ja = threadIdx.y;
+    int ic = blockIdx.x;
+    int jc = blockIdx.y;
+
+    const float* t = d + nn * nn;
+
+    float v[8][8];
+    for (int ib = 0; ib < 8; ++ib) {
+        for (int jb = 0; jb < 8; ++jb) {
+            v[ib][jb] = HUGE_VALF;
+        }
+    }
+    for (int k = 0; k < n; ++k) {
+        float x[8];
+        float y[8];
+        for (int ib = 0; ib < 8; ++ib) {
+            int i = ic * 64 + ib * 8 + ia;
+            x[ib] = t[nn*k + i];
+        }
+        for (int jb = 0; jb < 8; ++jb) {
+            int j = jc * 64 + jb * 8 + ja;
+            y[jb] = d[nn*k + j];
+        }
+        for (int ib = 0; ib < 8; ++ib) {
+            for (int jb = 0; jb < 8; ++jb) {
+                v[ib][jb] = min(v[ib][jb], x[ib] + y[jb]);
+            }
+        }
+    }
+    for (int ib = 0; ib < 8; ++ib) {
+        for (int jb = 0; jb < 8; ++jb) {
+            int i = ic * 64 + ib * 8 + ia;
+            int j = jc * 64 + jb * 8 + ja;
+            if (i < n && j < n) {
+                r[n*i + j] = v[ib][jb];
+            }
+        }
+    }
+}
+__global__ void myppkernel(const float* r, float* d, int n, int nn) {
+    int ja = threadIdx.x;
+    int i = blockIdx.y;
+
+    float* t = d + nn * nn;
+
+    for (int jb = 0; jb < nn; jb += 64) {
+        int j = jb + ja;
+        float v = (i < n && j < n) ? r[n*i + j] : HUGE_VALF;
+        d[nn*i + j] = v;
+        t[nn*j + i] = v;
+    }
+}
+
+__global__ void mykernel_v4(float* r, const float* d, int n, int nn) {
+    int ia = threadIdx.x;
+    int ja = threadIdx.y;
+    int ic = blockIdx.x;
+    int jc = blockIdx.y;
+
+    const float* t = d + nn * nn;
+
+    __shared__ float xx[4][64];
+    __shared__ float yy[4][64];
+
+    float v[8][8];
+    for (int ib = 0; ib < 8; ++ib) {
+        for (int jb = 0; jb < 8; ++jb) {
+            v[ib][jb] = HUGE_VALF;
+        }
+    }
+    for (int ks = 0; ks < n; ks += 4) {
+        int ija = ja * 8 + ia;
+        int i = ic * 64 + ija;
+        int j = jc * 64 + ija;
+        for (int f = 0; f < 4; ++f) {
+            int k = ks + f;
+            xx[f][ija] = t[nn*k + i];
+            yy[f][ija] = d[nn*k + j];
+        }
+
+        __syncthreads();
+
+        #pragma unroll
+        for (int f = 0; f < 4; ++f) {
+            float y[8];
+            for (int jb = 0; jb < 8; ++jb) {
+                y[jb] = yy[f][jb * 8 + ja];
+            }
+            for (int ib = 0; ib < 8; ++ib) {
+                float x = xx[f][ib * 8 + ia];
+                for (int jb = 0; jb < 8; ++jb) {
+                    v[ib][jb] = min(v[ib][jb], x + y[jb]);
+                }
+            }
+        }
+
+        __syncthreads();
+    }
+    for (int ib = 0; ib < 8; ++ib) {
+        for (int jb = 0; jb < 8; ++jb) {
+            int i = ic * 64 + ib * 8 + ia;
+            int j = jc * 64 + jb * 8 + ja;
+            if (i < n && j < n) {
+                r[n*i + j] = v[ib][jb];
+            }
+        }
+    }
+}
+
+void step_v2(float* r, const float* d, int n) {
+    int nn = roundup(n, 64);
+
+    // Allocate memory & copy data to GPU
+    float* dGPU = NULL;
+    CHECK(cudaMalloc((void**)&dGPU, 2 * nn * nn * sizeof(float)));
+    float* rGPU = NULL;
+    CHECK(cudaMalloc((void**)&rGPU, n * n * sizeof(float)));
+    CHECK(cudaMemcpy(rGPU, d, n * n * sizeof(float), cudaMemcpyHostToDevice));
+
+    // Run kernel
+    {
+        dim3 dimBlock(64, 1);
+        dim3 dimGrid(1, nn);
+        myppkernel<<<dimGrid, dimBlock>>>(rGPU, dGPU, n, nn);
+        CHECK(cudaGetLastError());
+    }
+
+    // Run kernel
+    {
+        dim3 dimBlock(8, 8);
+        dim3 dimGrid(nn / 64, nn / 64);
+        mykernel_v4<<<dimGrid, dimBlock>>>(rGPU, dGPU, n, nn);
+        CHECK(cudaGetLastError());
+    }
+
+    // Copy data back to CPU & release memory
+    CHECK(cudaMemcpy(r, rGPU, n * n * sizeof(float), cudaMemcpyDeviceToHost));
+    CHECK(cudaFree(dGPU));
+    CHECK(cudaFree(rGPU));
+}
+
+
 void step(float* r, const float* d, int n) {
     // Allocate memory & copy data to GPU
     float* dGPU = NULL;
@@ -53,7 +213,7 @@ void step(float* r, const float* d, int n) {
     // Run kernel
     dim3 dimBlock(16, 16);
     dim3 dimGrid(divup(n, dimBlock.x), divup(n, dimBlock.y));
-    mykernel<<<dimGrid, dimBlock>>>(rGPU, dGPU, n);
+    mykernel_v2<<<dimGrid, dimBlock>>>(rGPU, dGPU, n);
     CHECK(cudaGetLastError());
 
     // Copy data back to CPU & release memory
@@ -78,7 +238,7 @@ int main() {
     static float r[n*n];
 
     auto start = std::chrono::high_resolution_clock::now();
-    step(r, d, n);
+    step_v2(r, d, n);
     auto end = std::chrono::high_resolution_clock::now();
     auto ms_int = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
     std::cout << ms_int.count() << " ms";
